21a1d6432fbfd8bdf23023c7fe240f93c45483ee
listingslab <listingslab@gmail.com>
2019-11-03 06:13:25 +1000

    makeJSON

---
+++ markdown/Backlog.md
@@ -0,0 +1,24 @@
+## Backlog (which is a posh name for a To Do list)
+
+### React
+
+- `Contact.jsx` create actual form
+- Wire up redux
+- Post JSON to the function and listen for & display response
+- Move menu button into view/page component
+- Email not required Dialog
+- Contact Fab
+- Contact view
+
+### Storybook
+
+- New story `SiteSelect.jsx`
+
+### Firebase functions
+
+- `firebase init` functions
+- create contact function
+- run function locally
+- deploy only functions
+- Email notification
+- Save data to firestore collection
---
+++ markdown/makeJSON.js
@@ -0,0 +1,42 @@
+/* 
+    Run this with node to create a json file which recursively 
+    describes the markdown in current dir 
+*/
+
+var fs = require('fs');
+var path = require('path');
+
+function makeJSON (params){
+    var success = true;
+    var result = {
+        success,
+        params,
+        dir: walk(`.`)
+    };
+    logResult (result);
+    fs.writeFileSync("dir.json", JSON.stringify(result))
+    return result;
+}
+
+var walk = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            results = results.concat(walk(file));
+        } else { 
+            results.push(file);
+        }
+    });
+    return results;
+}
+
+function logResult (result){
+    console.log ('\n');
+    console.log (result);
+    console.log ('\n');
+}
+
+makeJSON(`please makeJSON`);
